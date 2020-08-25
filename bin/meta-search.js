6d871b26cb363336328a1cef9c14ab40021cc6da
Jon Peck <jpeck@fluxsauce.com>
2020-08-25 11:28:51 -0700

    Correcting aliases

--- bin/meta-search.js
+++ bin/meta-search.js
@@ -1,11 +1,1 @@
-#!/usr/bin/env node
-
-const program = require('commander');
-
-let loaded = false;
-if (!loaded) {
-  program
-    .command('package', 'Find packages in package.json that match given criteria')
-    .parse(process.argv);
-  loaded = true;
-}
+meta-search
